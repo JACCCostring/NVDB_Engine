d8369218f6601da3a91ffe816f5c41f97ec2cf29
JACC <93591202+JACCCostring@users.noreply.github.com>
2024-01-24 20:21:16 +0100

    Delete build directory

--- build/app/CMakeFiles/example_cpp_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/example_cpp_autogen"
-  "example_cpp_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/example_cpp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/app/CMakeFiles/nvdb.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for nvdb.
-# This may be replaced when dependencies are built.
